4fdc4a6a965ba64fbf848cb62a0cb281983cf6d1
Bryan Ward <wardbryan3@gmail.com>
2026-05-09 13:40:38 -0700

    refactor: use FastfetchWindow in HeroDashboard

--- src/components/HeroDashboard.jsx
+++ src/components/HeroDashboard.jsx
@@ -1,14 +1,12 @@
 import ParticleField from './ParticleField';
-import TitleOverlay from './TitleOverlay';
-import DashboardGrid from './DashboardGrid';
+import FastfetchWindow from './FastfetchWindow';
 
 export default function HeroDashboard({ projectCount = 0, postCount = 0 }) {
   return (
     <section className="hero-dashboard">
       <ParticleField />
       <div className="hero-content">
-        <TitleOverlay />
-        <DashboardGrid projectCount={projectCount} postCount={postCount} />
+        <FastfetchWindow projectCount={projectCount} postCount={postCount} />
       </div>
     </section>
   );
