fb5fe34d2ad983d09d13e4ba4686c1a0aabf42b8
Bryan Ward <wardbryan3@gmail.com>
2026-05-08 22:58:55 -0700

    feat: add RSS feed with @astrojs/rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection('blog');
+  return rss({
+    title: "Bryan Ward's Blog",
+    description: 'Thoughts on code, projects, and learning.',
+    site: context.site,
+    items: posts
+      .sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+      .map((post) => ({
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.date,
+        link: `/blog/${post.slug}`,
+      })),
+    customData: '<language>en-us</language>',
+  });
+}
