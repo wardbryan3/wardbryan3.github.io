14aa592594ba0b18dfd39043e223252dab530910
Bryan Ward <wardbryan3@gmail.com>
2026-05-09 06:54:36 -0700

    feat: add @astrojs/react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://bryanward.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
 });
